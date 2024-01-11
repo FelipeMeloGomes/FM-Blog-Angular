6b4f374732e7d37941cc81c2bc027a373e1d52ff
Felipe Melo <felipemelogomes36@gmail.com>
2024-01-11 02:03:34 -0300

    feat: projeto finalizado

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,28 @@
+export const dataFake = [
+  {
+    id: '1',
+    title: 'Homem de ferro',
+    description:
+      'Desde sua estreia em 2008, o Homem de Ferro tem sido um pilar fundamental no Universo Cinematográfico da Marvel (MCU). A trajetória de Tony Stark, interpretado brilhantemente por Robert Downey Jr., é repleta de reviravoltas, ação intensa e desenvolvimento de personagens.',
+    photoCover:
+      'https://media.contentapi.ea.com/content/dam/news/www-ea/common/ea-motive-new-title-teaser-16x9.jpg.adapt.1456w.jpg',
+  },
+  {
+    id: '2',
+    title: 'saiu novo filme',
+    description: 'ola',
+    photoCover: '',
+  },
+  {
+    id: '3',
+    title: 'saiu novo filme',
+    description: 'ola',
+    photoCover: '',
+  },
+  {
+    id: '4',
+    title: 'saiu novo filme',
+    description: 'ola',
+    photoCover: '',
+  },
+];
